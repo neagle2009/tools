0477f67a0758decffc94bf1f7f030e6499e921dc
neagle <gu217@126.com>
2019-07-13 10:26:51 +0800

    grub/main.go format

--- go/grub/main.go
+++ go/grub/main.go
@@ -8,15 +8,15 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-    "time"
+	"time"
 )
 
 const (
-	BASEURL  = "http://www.bz55.com"
-    //DOWNPATH = "/Users/didi/goproj/src/grub/image/"
-	DOWNPATH = "/home/neagle/goproj/src/grub/image/"
-    CHANNEL_CACHE_LENGTH = 10
-    CHANNEL_DOWN_QUEUE_LENGTH = 2
+	BASEURL = "http://www.bz55.com"
+	//DOWNPATH = "/Users/didi/goproj/src/grub/image/"
+	DOWNPATH                  = "/home/neagle/goproj/src/grub/image/"
+	CHANNEL_CACHE_LENGTH      = 10
+	CHANNEL_DOWN_QUEUE_LENGTH = 2
 )
 
 var urlChl chan string
@@ -32,7 +32,7 @@ func findElement(pageUrl string) {
 		src, find := s.Attr("src")
 		if find {
 			urlChl <- src
-			logOut(fmt.Sprintf("parse down url: %s, channel length: %d" ,src, len(urlChl)))
+			logOut(fmt.Sprintf("parse down url: %s, channel length: %d", src, len(urlChl)))
 		}
 	})
 }
@@ -41,8 +41,8 @@ func downFile(fileUrl string) {
 	fullUrl := BASEURL + fileUrl
 	//path, filename := filepath.Split(fullUrl)
 	_, filename := filepath.Split(fullUrl)
-    file := DOWNPATH + filename
-    logOut(file + " now is downloading ... ...")
+	file := DOWNPATH + filename
+	logOut(file + " now is downloading ... ...")
 	res, err := http.Get(fullUrl)
 	if err != nil {
 		log.Fatal("down file error, url:" + fullUrl)
@@ -54,52 +54,52 @@ func downFile(fileUrl string) {
 	}
 	defer f.Close()
 	io.Copy(f, res.Body)
-    logOut(file + " now is download finished")
+	logOut(file + " now is download finished")
 }
 
 func outCacheChannel() {
-    i := 0
+	i := 0
 	for {
-        i++
-        logOut(fmt.Sprintf("cache loop: %d, channel length:%d", i, len(urlChl)))
+		i++
+		logOut(fmt.Sprintf("cache loop: %d, channel length:%d", i, len(urlChl)))
 		select {
-		    case url := <-urlChl:
-                downChl <- url
+		case url := <-urlChl:
+			downChl <- url
 		}
 	}
 }
 
 func downChannel() {
-    i := 0
-    for {
-        i++
-        logOut(fmt.Sprintf("down loop: %d, channel length:%d", i, len(downChl)))
-        select {
-            case url := <-downChl:
-                downFile(url)
-        }
-    }
+	i := 0
+	for {
+		i++
+		logOut(fmt.Sprintf("down loop: %d, channel length:%d", i, len(downChl)))
+		select {
+		case url := <-downChl:
+			downFile(url)
+		}
+	}
 }
 
 func logOut(s string) {
-    log.Printf("%+v\t%s\n", time.Now().Format("2006-01-02 15:04:05"), s)
+	log.Printf("%+v\t%s\n", time.Now().Format("2006-01-02 15:04:05"), s)
 }
 
 func main() {
-    urls := []string{
-        "http://www.bz55.com/meinvbizhi/38925.html",
-        "http://www.bz55.com/meinvbizhi/38872.html",
-        "http://www.bz55.com/meinvbizhi/38818.html",
-        "http://www.bz55.com/meinvbizhi/38726.html",
-        "http://www.bz55.com/meinvbizhi/38444.html",
-    }
+	urls := []string{
+		"http://www.bz55.com/meinvbizhi/38925.html",
+		"http://www.bz55.com/meinvbizhi/38872.html",
+		"http://www.bz55.com/meinvbizhi/38818.html",
+		"http://www.bz55.com/meinvbizhi/38726.html",
+		"http://www.bz55.com/meinvbizhi/38444.html",
+	}
 
 	go outCacheChannel()
-    go downChannel()
+	go downChannel()
 	urlChl = make(chan string, CHANNEL_CACHE_LENGTH)
-    downChl = make(chan string, CHANNEL_DOWN_QUEUE_LENGTH)
-    for i, url := range urls {
-        fmt.Printf("%v\t%#v\n", i, string(url) )
-        findElement(string(url))
-    }
+	downChl = make(chan string, CHANNEL_DOWN_QUEUE_LENGTH)
+	for i, url := range urls {
+		fmt.Printf("%v\t%#v\n", i, string(url))
+		findElement(string(url))
+	}
 }
